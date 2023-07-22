13a6a07c583168eee4e74a0ed3aeb979982ca947
keerthana <keerthananeeluru2001@gmail.com>
2023-07-22 18:44:05 +0530

    commit:1

---
+++ Section_4/Section_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section_4
+{
+    internal class Program
+    {
+        public static void readfile(string fname, string path)
+        {
+            string fpath = path + fname;
+            /*StreamReader sr = new StreamReader(fpath);
+            string text = "";
+            while ((text = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(text);
+            }
+            sr.Close();*/
+            if (File.Exists(fpath))
+            {
+                string[] lines = File.ReadAllLines(fpath);
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Keerthana\\OneDrive\\Desktop\\job\\simpli\\Projects\\Section_4\\";
+            string fname = "stu_data.txt";
+            readfile(fname, path);
+            Console.ReadKey();
+
+        }
+    }
+}
